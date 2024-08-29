7c37706bc2fdf17df4cb50e1676c8aaec571058c
Ali-Shameem-V <alishameemv@gmail.com>
2024-08-29 22:09:14 +0530

    completed guid generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { v4 as uuidv4 } from 'uuid';  // Correct import for uuidv4
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Guid-Generator';
+export class AppComponent implements OnInit {
+  title = 'guidId-generator';
+  generatedUUID: string;
+  copySuccess: boolean = false;
+  showCopiedMessage= false;
+
+  constructor(private renderer: Renderer2) {
+    // Generate UUID on component initialization
+    this.generatedUUID = this.generateUUID();
+  }
+  ngOnInit(): void {
+    this.generatedUUID=this.generateUUID();
+  }
+
+  generateUUID(): string {
+    return uuidv4();
+  }
+
+  regenerateUUID() {
+    debugger
+    this.showCopiedMessage = false;
+    this.generatedUUID = this.generateUUID();
+  }
+
+  clipBoardCopy(){
+    debugger
+    const inputElement = document.getElementById('uuid') as HTMLInputElement;
+    this.showCopiedMessage = true;
+    inputElement.select();
+    document.execCommand('copy');
+    this.renderer.selectRootElement(inputElement).blur(); 
+  }
 }
